73cfb326a042503c485a1565438fdaba63cc158e
fabio-freitas-blueshift <137441951+fabio-freitas-blueshift@users.noreply.github.com>
2023-09-26 20:49:46 +0000

    teste http

--- shared_code/connection.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def getPropertiesMysql(database):
-    if database == "adm":
-        return os.environ["ADM_STRING_CONNECTION"]
-    if database == "portal":
-        return os.environ["ADM_STRING_CONNECTION"]
---
+++ teste_conexao/__init__.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+import logging
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        engine = create_engine(getPropertiesMysql("mysql+pymysql://usr_clockify_dev@clockifyadmdevblueshift:42Xxor6AFFnV@clockifyadmdevblueshift.mysql.database.azure.com:3306/administrativo_staging2"))
+        return func.HttpResponse(f"Engine Criada com sucesso")
+    except SQLAlchemyError as e:
+        return func.HttpResponse(f"Herror, {e}.")
+    finally:
+        if engine:
+            engine.dispose()
+
+    
