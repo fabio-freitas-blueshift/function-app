10c1099a51f285b215ae0db0b32ed8ec32343aa4
fabio-freitas-blueshift <137441951+fabio-freitas-blueshift@users.noreply.github.com>
2023-09-26 20:59:10 +0000

    teste engine

--- teste_conexao/__init__.py
+++ teste_conexao/__init__.py
@@ -5,7 +5,7 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        engine = create_engine(getPropertiesMysql("mysql+pymysql://usr_clockify_dev@clockifyadmdevblueshift:42Xxor6AFFnV@clockifyadmdevblueshift.mysql.database.azure.com:3306/administrativo_staging2"))
+        engine = create_engine("mysql+pymysql://usr_clockify_dev@clockifyadmdevblueshift:42Xxor6AFFnV@clockifyadmdevblueshift.mysql.database.azure.com:3306/administrativo_staging2")
         return func.HttpResponse(f"Engine Criada com sucesso")
     except SQLAlchemyError as e:
         return func.HttpResponse(f"Herror, {e}.")
